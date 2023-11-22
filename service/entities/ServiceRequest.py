ef351cb6d68f35085bef8260f00b207d895e684d
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2023-11-22 11:16:24 +0330

    Added customer and accepted_specialist fields to ServiceRequest model and specialist field to ServiceRequestSpecialist model

--- service/entities/ServiceRequest.py
+++ service/entities/ServiceRequest.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.db.models import JSONField
 from service.entities import ServiceRequestStatus
+from service.entities.Specialty import Specialty
+from user.models import Customer, Specialist
 
 
 class ServiceRequest(models.Model):
-    specialty = models.ForeignKey('Specialty', on_delete=models.CASCADE)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE, null=True)
     description = models.TextField()
     address = models.CharField(max_length=255)
     reception_date = models.DateTimeField()
-    # customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
-    # accepted_specialist = models.ForeignKey('Specialist', on_delete=models.CASCADE, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    accepted_specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE, null=True, blank=True)
     status = models.CharField(
         max_length=50,
         choices=ServiceRequestStatus.ServiceRequestStatus.choices,  # Use .choices here
--- service/entities/ServiceRequestSpecialist.py
+++ service/entities/ServiceRequestSpecialist.py
@@ -1,11 +1,12 @@
 from django.db import models
 from service.entities import ServiceRequestSpecialistStatus
 from service.entities.ServiceRequest import ServiceRequest
+from user.models import Specialist
 
 
 class ServiceRequestSpecialist(models.Model):
-    # specialist = models.ForeignKey('Specialist', on_delete=models.CASCADE)
-    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE)
+    specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE, null=True)
+    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE, null=True)
     status = models.CharField(
         max_length=50,
         choices=ServiceRequestSpecialistStatus.ServiceRequestSpecialistStatus.choices,
