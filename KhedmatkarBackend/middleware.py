2ae68accea91ae336cf3217a478adc860442f3f6
erfanfi79 <erfanfaravani@gmail.com>
2024-02-01 12:33:28 +0330

    Disable Csrf

---
+++ KhedmatkarBackend/middleware.py
@@ -0,0 +1,12 @@
+from KhedmatkarBackend import settings
+
+
+class DisableCSRFOnDebug(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        setattr(request, '_dont_enforce_csrf_checks', True)
+        response = self.get_response(request)
+        return response
