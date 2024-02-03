f9da07088eccb86bb82288d45321c3d5048391e1
Alireza Babazadeh <alireza_bz@yahoo.com>
2024-02-03 11:36:10 +0330

    fix: fix

--- payment/urls.py
+++ payment/urls.py
@@ -3,7 +3,7 @@ from .views import WalletListAPIView, WalletDetailAPIView, AdminWalletDetailAPIV
 
 urlpatterns = [
     path('wallets/', WalletListAPIView.as_view()),
-    path('wallets/<int:pk>/', WalletDetailAPIView.as_view()),
+    path('wallets/', WalletDetailAPIView.as_view()),
     path('wallets/admin/<int:pk>/', AdminWalletDetailAPIView.as_view()),
     path('transactions/', TransactionListAPIView.as_view()),
     path('transactions/<int:pk>/', TransactionDetailAPIView.as_view()),
