db7c0e9c2787bbe886c8684205fd803aec816697
Alireza Babazadeh <alireza_bz@yahoo.com>
2024-02-03 11:38:08 +0330

    fix: fix

--- payment/urls.py
+++ payment/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import WalletListAPIView, WalletDetailAPIView, AdminWalletDetailAPIView, TransactionListAPIView, TransactionDetailAPIView
 
 urlpatterns = [
-    path('wallets/', WalletListAPIView.as_view()),
+    path('wallets/create/', WalletListAPIView.as_view()),
     path('wallets/', WalletDetailAPIView.as_view()),
     path('wallets/admin/<int:pk>/', AdminWalletDetailAPIView.as_view()),
     path('transactions/', TransactionListAPIView.as_view()),
